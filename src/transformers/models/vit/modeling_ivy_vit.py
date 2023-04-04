8d0c43739ed931dd334b05fd6d6318251309baa2
smilla.fox <smilla.fox@student.hpi.uni-potsdam.de>
2023-04-04 08:12:45 +0200

    add patch embeddings class in Ivy

---
+++ src/transformers/models/vit/modeling_ivy_vit.py
@@ -0,0 +1,43 @@
+import collections.abc
+import ivy
+from .configuration_vit import ViTConfig
+
+
+class IvyViTPatchEmbeddings(ivy.Module):
+    """
+    This class turns `pixel_values` of shape `(batch_size, num_channels, height, width)` into the initial
+    `hidden_states` (patch embeddings) of shape `(batch_size, seq_length, hidden_size)` to be consumed by a
+    Transformer.
+    """
+
+    def __init__(self, config):
+        super().__init__()
+        image_size, patch_size = config.image_size, config.patch_size
+        num_channels, hidden_size = config.num_channels, config.hidden_size
+
+        image_size = image_size if isinstance(image_size, collections.abc.Iterable) else (image_size, image_size)
+        patch_size = patch_size if isinstance(patch_size, collections.abc.Iterable) else (patch_size, patch_size)
+        num_patches = (image_size[1] // patch_size[1]) * (image_size[0] // patch_size[0])
+        self.image_size = image_size
+        self.patch_size = patch_size
+        self.num_channels = num_channels
+        self.num_patches = num_patches
+
+        self.projection = ivy.Conv2D(num_channels, hidden_size, filter_shape=patch_size, strides=patch_size, data_format="NCHW")
+
+    def forward(self, pixel_values: ivy.Array, interpolate_pos_encoding: bool = False) -> ivy.Array:
+        batch_size, num_channels, height, width = pixel_values.shape
+        if num_channels != self.num_channels:
+            raise ValueError(
+                "Make sure that the channel dimension of the pixel values match with the one set in the configuration."
+            )
+        if not interpolate_pos_encoding:
+            if height != self.image_size[0] or width != self.image_size[1]:
+                raise ValueError(
+                    f"Input image size ({height}*{width}) doesn't match model"
+                    f" ({self.image_size[0]}*{self.image_size[1]})."
+                )
+
+        embeddings = self.projection(pixel_values)
+        embeddings = ivy.swapaxes(ivy.flatten(embeddings, start_dim=2), 1, 2)
+        return embeddings
